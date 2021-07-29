cd7e961ab6aba281b867ba84dd616d1bd1d02fce
James jr <jameslu91223@gmail.com>
2021-07-29 18:05:38 +0800

    add a simple banner for start

---
+++ src/components/NFTBanner.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="nft-banner border-2 rounded-2xl w-full flex">
+    <div class="w-7/12  h-72 grid place-items-center">
+      <h1 class="w-full text-4xl mb-6 text-center">
+        Won NFTs via donation,without Cost!
+      </h1>
+      <div class=" ">
+        <div class="flex">
+          <div
+            class="w-48 p-3 border-2 rounded-2xl text-center mx-16 cursor-pointer bg-green-500 hover:bg-green-700  "
+          >
+            <span class="text-bold">Explore NFTs</span>
+          </div>
+          <div
+            class="w-48 p-3 border-2 rounded-2xl text-center mx-16 cursor-pointer bg-blue-500 hover:bg-blue-700  "
+          >
+            <span class="text-bold">Mint NFTs</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class=" w-5/12 grid place-items-center">
+      <img
+        src="https://lh3.googleusercontent.com/ZiQyaBbrcRyxYRN2MXW9b59zXVM6wNxCIYGXXebOE7hTNyD75Ysn9CHXh-2eRfRN1F1bybbR0UogzSdO01bNEejIx6dzcvLB9YdiRqc=s264"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1 class="my-12 text-2xl font-bold">
+      Lending a hand
+    </h1>
+    <NFTBanner />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NFTBanner from "@/components/NFTBanner.vue";
+
+export default {
+  name: "Home",
+  components: {
+    NFTBanner
+  }
+};
+</script>
+<style scoped>
+.home {
+  margin: 0px auto;
+  max-width: min(1280px, 100% - 40px);
+  width: 100%;
+  flex-wrap: wrap;
+}
+</style>
